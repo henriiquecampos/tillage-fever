bfabd0da109cd022707451f1cc6817fd5a7b77a5
Henrique Campos <henriiquecampos@gmail.com>
2017-12-02 14:55:59 -0200

    Added cells increasing player score

--- source/areas/field/field.gd
+++ source/areas/field/field.gd
@@ -2,6 +2,9 @@ extends TileMap
 
 var last_cell = Vector2(15,15)
 var current_tile = 1
+
+func _ready():
+	print()
 func _process(delta):
 	if Input.is_action_pressed("click"):
 		var current_cell = world_to_map(get_local_mouse_position())
@@ -16,13 +19,17 @@ func set_new_tile(cell):
 		if ((get_cellv(cell)) == 0) and (player.current_score - player.tile_price >= 0):
 			set_cellv(cell, 1)
 			player.set_current_score(player.current_score - player.tile_price)
+			player.set_supply(player.supply + 10)
 		else:
 			set_cellv(cell, 0)
 			player.set_current_score(int(player.current_score + (player.tile_price/2)))
+			player.set_supply(player.supply - 10)
 			
 func changed_tiles(days):
-	#change the tiles according to the current amount of days
 	for c in get_used_cells():
-		print(get_cellv(c))
-		if get_cellv(c) >=1:
+		if get_cellv(c) >= (get_tileset().get_tiles_ids().size() -1):
+			set_cellv(c, 0)
+			player.set_supply(player.supply - 10)
+			player.set_current_score(player.current_score + (player.tile_price * 2))
+		elif get_cellv(c) >=1:
 			set_cellv(c, get_cellv(c) + 1)
