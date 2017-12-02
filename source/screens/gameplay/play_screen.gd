7408fb7a0a61f933ec32dd4423fd4b8ba29a44c0
Henrique Campos <henriiquecampos@gmail.com>
2017-12-02 12:10:20 -0200

    Added timing behavior for gameplay screen

--- source/screens/gameplay/play_screen.gd
+++ source/screens/gameplay/play_screen.gd
@@ -7,13 +7,24 @@ onready var score_text = score.get_text()
 onready var initial_score = player.current_score
 
 var current_time = 0.0
-var days = 90 setget set_days
+var days = 90
 
 func _ready():
 	player.connect("score_change", self, "update_score")
 	time.set_text(time_text.format({"amount":days}))
 	score.set_text(score_text.format({"amount":initial_score}))
 
+func _process(delta):
+	if days >= 0:
+		current_time += delta
+		if current_time >= 5.0:
+			days -= 1
+			update_days(days)
+			current_time = 0.0
 
 func update_score(value):
 	score.set_text(score_text.format({"amount":value}))
+	
+
+func update_days(value):
+	time.set_text(time_text.format({"amount":value}))
