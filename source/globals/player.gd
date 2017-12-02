34855a135ff1547d73c5da7d6a24d932ec419c70
Henrique Campos <henriiquecampos@gmail.com>
2017-12-02 14:56:38 -0200

    Added supply and demand methods for player

--- source/globals/player.gd
+++ source/globals/player.gd
@@ -1,10 +1,22 @@
 extends Node
 
-export (int) var current_score = 1200 setget set_current_score
+export (int) var current_score = 10000000 setget set_current_score
 export (int) var tile_price = 200
+var supply = 0 setget set_supply
+var demand = 100 setget set_demand
 
-signal score_change(score)
+signal score_changed(value)
+signal supply_changed(value)
+signal demand_changed(value)
 
 func set_current_score(value):
 	current_score = value
 	emit_signal("score_change", current_score)
+	
+func set_supply(value):
+	supply = value
+	emit_signal("supply_changed", supply)
+
+func set_demand(value):
+	demand = value
+	emit_signal("demand_changed", demand)
