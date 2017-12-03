6b62a5e9b95107d575cdafd4f62b55cf59c1d69d
Henrique Campos <henriiquecampos@gmail.com>
2017-12-03 13:43:38 -0200

    Changed Gauge sprites and Added animations

--- source/objects/gauge/gauge.gd
+++ source/objects/gauge/gauge.gd
@@ -3,12 +3,20 @@ extends TextureProgress
 func _ready():
 	player.connect("demand_changed", self, "update_demand")
 	player.connect("supply_changed", self, "update_supply")
+	set_max(player.demand)
+	set_value(player.supply)
 	
 func update_demand(demand):
-	set_max(demand)
+	var t = $Tween
+	t.interpolate_method(self, "set_max", get_max(), demand, 0.5,
+	t.TRANS_ELASTIC, t.EASE_OUT)
+	t.start()
 	
 func update_supply(supply):
-	set_value(supply)
+	var t = $Tween
+	t.interpolate_method(self, "set_value", get_value(), supply, 0.5,
+	t.TRANS_ELASTIC, t.EASE_OUT)
+	t.start()
 
 func check_supply_demand():
 	var spread = max_value - value
@@ -16,3 +24,7 @@ func check_supply_demand():
 	var dif = (abs(spread - (value*100/max_value)))
 	if dif >= 30:
 		get_parent().change_scene("res://screens/lose/lose_screen.tscn")
+	if player.supply > player.demand:
+		player.tile_price -= 25
+	elif player.supply < player.demand:
+		player.tile_price += 25
