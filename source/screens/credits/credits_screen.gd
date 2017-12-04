ca5e6a2eda3a316e534a7c3c5d66f67463918642
Henrique Campos <henriiquecampos@gmail.com>
2017-12-04 14:23:19 -0200

    Release (#23)
    * Added tile assets to project
    
    * Updated Tilemap resource
    
    * Changed Gauge sprites and Added animations
    
    * Added visual feedback for BUY and SELL
    
    Also increased field's size by 1 in Y axis
    
    * Added set function for tile_price in player
    
    Also added a quit button in main screen
    
    * Changed tileset pallette
    
    * Changed gaunge pallette
    
    * Added theme to Screens
    
    * Added theme for HSliders and Added Title
    
    * Added Particles singleton
    
    * Added BGM Asset and editable
    
    * Changed Options Behavior to reflect Buses volume
    
    * Added cap value for demand
    
    * Added randomize and Recursive update
    
    * Removed export keywords
    
    * Fixed issue where players could add new tiles to Tilemap
    
    * Added main screen scene path to Win Screen
    
    * Added Sound Effects editable
    
    * Added Sound Effects
    
    * Implemented Sound Effects
    
    * Added warning to gauge and Fixed spread calc
    
    * Finished polishing
    
    * Added Pigdev logo to Credits
    
    * Added price feedback for gauge
    
    * Fixed demand being more than field total supply
    
    * Finished Lose Screen
    
    * Finished Win Screen

--- source/screens/credits/credits_screen.gd
+++ source/screens/credits/credits_screen.gd
@@ -4,9 +4,21 @@ func _ready():
 	$AnimationPlayer.play("credits")
 	$Back.connect("button_up", self, "_on_button_down")
 	$ItchButton.connect("button_up", self, "_on_button_down")
+	$ItchButton.connect("button_up", self, "_on_itch_button_down")
+
 	
 func _on_back_button():
 	change_scene()
 
 func _on_itch_button():
+	$ItchButton.set_self_modulate(Color(1.0,1.0,1.0,1.0))
 	OS.shell_open("https://pigdev.itch.io")
+
+func _on_itch_button_down():
+	$ItchButton.set_self_modulate(Color(0.7,0.7,0.7,1.0))
+
+func _on_itch_mouse_entered():
+	$ItchButton.set_self_modulate(Color(1.1,1.1,1.1,1.0))
+
+func _on_itch_mouse_exited():
+	$ItchButton.set_self_modulate(Color(1.0,1.0,1.0,1.0))
