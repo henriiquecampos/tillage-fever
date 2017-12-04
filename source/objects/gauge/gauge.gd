f8f9e3df1faa70c683f2f6b9b14a93647a58dd7e
Henrique Campos <henriiquecampos@gmail.com>
2017-12-04 11:24:22 -0200

    Added warning to gauge and Fixed spread calc

--- source/objects/gauge/gauge.gd
+++ source/objects/gauge/gauge.gd
@@ -5,12 +5,14 @@ func _ready():
 	player.connect("supply_changed", self, "update_supply")
 	set_max(player.demand)
 	set_value(player.supply)
-	
+	check_warning()
+
 func update_demand(demand):
 	var t = $Tween
 	t.interpolate_method(self, "set_max", get_max(), demand, 0.5,
 	t.TRANS_ELASTIC, t.EASE_OUT)
 	t.start()
+	check_warning()
 
 func update_supply(supply):
 	var t = $Tween
@@ -19,14 +21,26 @@ func update_supply(supply):
 	t.start()
 	sfx_player.set_stream(sfx_player.sup_dem)
 	sfx_player.play()
+	check_warning()
 
 func check_supply_demand():
-	var spread = max_value - value
-	spread = (spread * 100)/max_value
-	var dif = (abs(spread - (value*100/max_value)))
-	if dif >= 30:
+	if calc_spread() < 30 or calc_spread() > 70:
 		get_parent().change_scene("res://screens/lose/lose_screen.tscn")
 	if player.supply > player.demand:
 		player.tile_price -= 25
 	elif player.supply < player.demand:
 		player.tile_price += 25
+		
+func check_warning():
+	if calc_spread() < 30 or calc_spread() > 70:
+		$Warning.show()
+		$Warning/AnimationPlayer.play("warning")
+	else:
+		$Warning.hide()
+		$Warning/AnimationPlayer.stop()
+		
+func calc_spread():
+	var dif = abs(player.demand - player.supply)
+	var spread = (dif * 100)/player.demand
+	print(spread)
+	return(spread)
